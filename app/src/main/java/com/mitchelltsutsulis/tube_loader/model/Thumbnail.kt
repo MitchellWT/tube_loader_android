eb4ecf13c476b99b06a3918a0e04de639236f50a
Mitchell <mitchell444999@gmail.com>
2022-09-10 22:32:17 +1000

    Formatting fix and added attributes to video obj to mirror backend

--- app/src/main/java/com/mitchelltsutsulis/tube_loader/model/Thumbnail.kt
+++ app/src/main/java/com/mitchelltsutsulis/tube_loader/model/Thumbnail.kt
@@ -4,5 +4,8 @@ import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
-data class Thumbnail(val source: String, val width: String = "480",
-                     val height: String = "360"): Parcelable
+data class Thumbnail(
+    val source: String,
+    val width: String = "480",
+    val height: String = "360"
+) : Parcelable
