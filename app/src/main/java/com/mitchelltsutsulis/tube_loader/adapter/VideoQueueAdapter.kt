bcb6857f6dfcbb2ac4c404d4cb0e8121847eb1f7
Mitchell <mitchell444999@gmail.com>
2022-09-10 22:30:23 +1000

    Added refactoring for all adapters

--- app/src/main/java/com/mitchelltsutsulis/tube_loader/adapter/VideoQueueAdapter.kt
+++ app/src/main/java/com/mitchelltsutsulis/tube_loader/adapter/VideoQueueAdapter.kt
@@ -13,52 +13,40 @@ import com.mitchelltsutsulis.tube_loader.R
 import com.mitchelltsutsulis.tube_loader.model.Video
 import com.squareup.picasso.Picasso
 
-class VideoQueueAdapter(private val data: MutableList<Video>,
-                        private val context: Context?,
-                        private val listener: (Video) -> Unit): RecyclerView.Adapter<VideoQueueAdapter.ViewHolder>() {
+class VideoQueueAdapter(
+    private val data: MutableList<Video>,
+    private val context: Context,
+    private val listener: (Video) -> Unit
+) : RecyclerView.Adapter<VideoQueueAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.video_queue_layout, parent, false) as View
-
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.video_queue_layout, parent, false)
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int = data.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
-        holder.bind(item)
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(data[position])
 
     fun getItemIndex(item: Video) = data.indexOf(item)
 
-    inner class ViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
-        // Getting components from view
-        private val queued: Button       = view.findViewById(R.id.queue_button)
-        private val title: TextView      = view.findViewById(R.id.title)
-        private val thumbnail: ImageView = view.findViewById(R.id.thumbnail)
-        // Set initial value of button text
-        private val buttonText = MutableLiveData(context?.getString(R.string.queued_button))
+    inner class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+        private val queued = view.findViewById<Button>(R.id.queue_button)
+        private val title = view.findViewById<TextView>(R.id.title)
+        private val thumbnail = view.findViewById<ImageView>(R.id.thumbnail)
+        private val buttonText = MutableLiveData(context.getString(R.string.queued_button))
 
-        // Sets component data from passed in video
         fun bind(item: Video) {
-            // Conditional for button text
-            buttonText.value = if (!item.queued) context?.getString(R.string.un_queued_button)
-            else context?.getString(R.string.queued_button)
-
+            title.text = item.title
+            buttonText.value = context.getString(
+                if (item.queued) R.string.queued_button else R.string.un_queued_button
+            )
             queued.text = buttonText.value
-            title.text  = item.title
             Picasso.get().load(item.thumbnail.source)
                 .placeholder(R.drawable.ic_black)
                 .error(R.drawable.thumbnail_not_found)
                 .into(thumbnail)
-            // Old code
-            // (context as App).loadBitmap(item.videoId, thumbnail)
-
-            // Sets on click listener, function provided during instantiation
-            queued.setOnClickListener {
-                listener(item)
-            }
+            queued.setOnClickListener { listener(item) }
         }
     }
 }
--- app/src/main/java/com/mitchelltsutsulis/tube_loader/adapter/VideoSearchAdapter.kt
+++ app/src/main/java/com/mitchelltsutsulis/tube_loader/adapter/VideoSearchAdapter.kt
@@ -1,6 +1,5 @@
 package com.mitchelltsutsulis.tube_loader.adapter
 
-import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,43 +10,31 @@ import com.mitchelltsutsulis.tube_loader.R
 import com.mitchelltsutsulis.tube_loader.model.Video
 import com.squareup.picasso.Picasso
 
-class VideoSearchAdapter(private val data: List<Video>,
-                         // Old code
-                         // private val context: Context?,
-                         private val listener: (Video) -> Unit): RecyclerView.Adapter<VideoSearchAdapter.ViewHolder>() {
+class VideoSearchAdapter(
+    private val data: List<Video>,
+    private val listener: (Video) -> Unit
+) : RecyclerView.Adapter<VideoSearchAdapter.ViewHolder>() {
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
-        val layoutInflater = LayoutInflater.from(parent.context)
-        val view = layoutInflater.inflate(R.layout.video_search_layout, parent, false) as View
-
+        val inflater = LayoutInflater.from(parent.context)
+        val view = inflater.inflate(R.layout.video_search_layout, parent, false)
         return ViewHolder(view)
     }
 
     override fun getItemCount(): Int = data.size
 
-    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val item = data[position]
-        holder.bind(item)
-    }
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) = holder.bind(data[position])
 
-    inner class ViewHolder(private val view: View): RecyclerView.ViewHolder(view) {
-        // Getting components from view
-        private val title: TextView      = view.findViewById(R.id.title)
-        private val thumbnail: ImageView = view.findViewById(R.id.thumbnail)
+    inner class ViewHolder(private val view: View) : RecyclerView.ViewHolder(view) {
+        private val title = view.findViewById<TextView>(R.id.title)
+        private val thumbnail = view.findViewById<ImageView>(R.id.thumbnail)
 
-        // Sets component data from passed in video
         fun bind(item: Video) {
             title.text = item.title
             Picasso.get().load(item.thumbnail.source)
                 .placeholder(R.drawable.ic_black)
                 .error(R.drawable.thumbnail_not_found)
                 .into(thumbnail)
-            // Old code
-            // (context as App).loadBitmap(item.videoId, thumbnail)
-
-            // Sets on click listener, function provided during instantiation
-            view.setOnClickListener {
-                listener(item)
-            }
+            view.setOnClickListener { listener(item) }
         }
     }
 }
