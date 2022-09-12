a68c55917dfe283e7d0f342d4b2c16e1605da1c0
Mitchell <mitchell444999@gmail.com>
2022-09-12 20:06:13 +1000

    Moved resToRes func to static jsonSeqToList func

--- app/src/main/java/com/mitchelltsutsulis/tube_loader/model/Video.kt
+++ app/src/main/java/com/mitchelltsutsulis/tube_loader/model/Video.kt
@@ -1,6 +1,7 @@
 package com.mitchelltsutsulis.tube_loader.model
 
 import android.os.Parcelable
+import com.fasterxml.jackson.databind.JsonNode
 import kotlinx.parcelize.Parcelize
 
 @Parcelize
@@ -13,4 +14,27 @@ data class Video(
     val downloadedAt: String = "",
     val backendId: Int = -1,
     val directory: String = "",
-) : Parcelable
+) : Parcelable {
+    companion object {
+        fun jsonSeqToList(videos: Sequence<JsonNode>) = videos.map {
+            val backendId = it.get("id").asInt()
+            val downloaded = it.get("downloaded").asBoolean()
+            val queued = it.get("queued").asBoolean()
+            val videoId = it.get("video_id").asText()
+            val title = it.get("title").asText()
+            val thumbnail = it.get("thumbnail").asText()
+            val downloadedAt = it.get("downloaded_at").asText()
+            val directory = it.get("directory").asText()
+            Video(
+                videoId,
+                title,
+                Thumbnail(thumbnail),
+                queued,
+                downloaded,
+                downloadedAt,
+                backendId,
+                directory
+            )
+        }.toList()
+    }
+}
