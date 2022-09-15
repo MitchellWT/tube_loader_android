c9182e492091a863a2641a66ffa65a3adacb98d0
Mitchell <mitchell444999@gmail.com>
2022-09-16 01:16:41 +1000

    Added EncryptedSharedPreferences to application, with getters and setters

--- app/src/main/java/com/mitchelltsutsulis/tube_loader/App.kt
+++ app/src/main/java/com/mitchelltsutsulis/tube_loader/App.kt
@@ -1,15 +1,49 @@
 package com.mitchelltsutsulis.tube_loader
 
 import android.app.Application
-import java.util.*
+import android.content.SharedPreferences
+import androidx.core.content.edit
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
 
-class App: Application() {
-    lateinit var basicAuthStr: String
+class App : Application() {
+    private lateinit var masterKeyAlias: String
+    private lateinit var prefs: SharedPreferences
 
     override fun onCreate() {
         super.onCreate()
-        basicAuthStr = Base64.getEncoder().encodeToString(
-            "${getString(R.string.username)}:${getString(R.string.password)}".toByteArray()
+
+        masterKeyAlias = MasterKeys.getOrCreate(MasterKeys.AES256_GCM_SPEC)
+        prefs = EncryptedSharedPreferences.create(
+            "tube_loader_private",
+            masterKeyAlias,
+            applicationContext,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
         )
     }
+
+    fun loadData(
+        authToken: String,
+        youtubeToken: String,
+        serverScheme: String,
+        serverAuthority: String
+    ) = prefs.edit {
+        putString("auth_token", authToken)
+        putString("youtube_token", youtubeToken)
+        putString("server_scheme", serverScheme)
+        putString("server_authority", serverAuthority)
+    }
+
+    fun deleteData() = prefs.edit {
+        putString("auth_token", "")
+        putString("youtube_token", "")
+        putString("server_scheme", "")
+        putString("server_authority", "")
+    }
+
+    fun getAuthToken() = prefs.getString("auth_token", "") ?: ""
+    fun getYoutubeToken() = prefs.getString("youtube_token", "") ?: ""
+    fun getServerScheme() = prefs.getString("server_scheme", "") ?: ""
+    fun getServerAuthority() = prefs.getString("server_authority", "")  ?: ""
 }
